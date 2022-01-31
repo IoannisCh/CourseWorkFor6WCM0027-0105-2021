275b5e82ee51d9dddd9b2f80a71907508d8c3290
IoannisCh <ioannis.chantolios@ieee.org>
2022-01-31 19:51:37 +0000

    created cases in castTest

--- CastTest/src/main/java/CastTest.java
+++ CastTest/src/main/java/CastTest.java
@@ -13,13 +13,31 @@ public class CastTest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        double d = 99.07;
-        long l = (long)d;
-        int i = (int)l;
-        
-        System.out.println("Double value "+d);
-        System.out.println("Long value "+l);
-        System.out.println("Int value "+i);
+        int myvar = 2;
+        switch (myvar){
+            case 1:
+                System.out.println("one");
+                break;
+              
+            case 2:
+                System.out.println("two");
+                break;
+                
+            case 3:
+                System.out.println("three");
+                break;
+                
+            case 4:
+                System.out.println("four");
+                break;
+                
+            case 5:
+                System.out.println("five");
+                break;
+                
+            case 6:
+                System.out.println("six");
+        }
     }
     
 }
