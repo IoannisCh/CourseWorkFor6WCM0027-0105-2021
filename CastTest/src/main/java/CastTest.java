f6221d04eabed748e39ac1976f22344c9693a44a
IoannisCh <ioannis.chantolios@ieee.org>
2022-01-31 19:34:10 +0000

    created two more projects, one with testing units

---
+++ CastTest/src/main/java/CastTest.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author yiannis
+ */
+public class CastTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double d = 99.07;
+        long l = (long)d;
+        int i = (int)l;
+        
+        System.out.println("Double value "+d);
+        System.out.println("Long value "+l);
+        System.out.println("Int value "+i);
+    }
+    
+}
---
+++ OurDemoTestBed/src/main/java/OurDemoTestBed.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author yiannis
+ */
+public class OurDemoTestBed {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //PAYLOAD BEGINS
+        
+        String firstPart = "Abraam";
+        String secondPart = "Lincoln";
+        String result = firstPart + " " + secondPart;
+        
+        //PAYLOAD ENDS
+        
+        System.out.println(result);
+    }
+    
+}
