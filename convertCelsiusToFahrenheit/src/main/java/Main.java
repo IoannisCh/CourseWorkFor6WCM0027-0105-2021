74bae1f9e91dab3ad03df69ce3b0847f9d1aeeb2
IoannisCh <ioannis.chantolios@ieee.org>
2022-01-31 21:46:37 +0000

    started Celsius to Fahrenheit program

---
+++ convertCelsiusToFahrenheit/src/main/java/Main.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author yiannis
+ */
+public class Main {
+    
+    static double convertCelciusToFahrenheit (double celcius){
+        return (celcius * 9 / 5) +32;
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double smallest = 0;
+        double largest = 100;
+        
+        for (double i = smallest; i <= largest; ++i){
+            System.out.println(i + " in fahrenheit = ");
+            System.out.println("%.2f", convertCelciusToFahrenheit(i));
+            System.out.println();
+        }
+    }
+    
+}
