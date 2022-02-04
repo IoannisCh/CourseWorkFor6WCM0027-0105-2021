318aa28ba5d8e1b90612ae001456d966607bcf8d
IoannisCh <ioannis.chantolios@ieee.org>
2022-02-04 19:21:23 +0000

    minor text corrections, file clean up

--- OurDemoTestBed/src/main/java/OurDemoTestBed.java
+++ OurDemoTestBed/src/main/java/OurDemoTestBed.java
@@ -1,17 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 
-/**
- *
- * @author yiannis
- */
 public class OurDemoTestBed {
 
-    /**
-     * @param args the command line arguments
-     */
+  
     public static void main(String[] args) {
         
         //PAYLOAD BEGINS
