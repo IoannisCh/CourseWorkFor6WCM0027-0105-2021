d5db31ef0d815c2ecb2a5dc185a99a0b9cb560f2
IoannisCh <ioannis.chantolios@ieee.org>
2022-04-16 11:41:24 +0100

    changes to student class, adding functionallity of display array

--- PTCManagement_System/src/ptcmanagement_system/Ptc_management_system.java
+++ PTCManagement_System/src/ptcmanagement_system/Ptc_management_system.java
@@ -1,10 +1,41 @@
 package ptcmanagement_system;
 
-public class Ptc_management_system {
+import java.util.Scanner;
+
+
+
+public class Ptc_management_system extends Student{
     public static void main(String[] args) {
-        Student stud1 = new Student();
-        stud1.enroll();
-        System.out.print(stud1.toString());
+    System.out.println("Enter a number: ");
+    Scanner in = new Scanner(System.in);
+    if (in.nextInt != 1){
+        Display();
+    } else {
+        Register();
+        }
+      
+            
+    }
+    private Object[] students;
+    public static void Register() {
+     System.out.print("Enter number of new enrollments: ");
+        Scanner in = new Scanner(System.in);
+        int numOfStudents = in.nextInt();
+        Student[] students = new Student[numOfStudents];
+    
+        for (int n = 0; n < numOfStudents; n++){
+            students[n] = new Student();
+            students[n].enroll();
+        }
     }
     
-}
+    public static void Display(){
+        int numOfStudents = 0;
+        for (int n = 0; n < numOfStudents; n++){
+            System.out.print(students[n].toString());
+        }
+    }
+       
+    }
+    
+
